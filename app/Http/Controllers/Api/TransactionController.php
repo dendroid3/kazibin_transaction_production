eff3ed6afbab2c22757005cabee58621ac0a718e
Denis Mwangi <denniswanjohi.m@gmail.com>
2023-08-23 21:04:16 +0300

    Mpesa integration

--- app/Http/Controllers/Api/TransactionController.php
+++ app/Http/Controllers/Api/TransactionController.php
@@ -5,12 +5,32 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Str;
+use Illuminate\Support\Facades\Log;
 use Carbon\Carbon;
 use Illuminate\Support\Facades\DB;
 
+use Mpesa;
 
 class TransactionController extends Controller
 {
+    public function getAccessToken()
+    {
+        $authorization = base64_encode(env('MPESA_CONSUMER_KEY') . ":" . env('MPESA_CONSUMER_SECRET'));
+        $ch = curl_init('https://sandbox.safaricom.co.ke/oauth/v1/generate?grant_type=client_credentials');
+        curl_setopt($ch, CURLOPT_HTTPHEADER, ['Authorization: Basic ' . $authorization]);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+        // curl_setopt($ch, CURLOPT_CAINFO, 'SandboxCertificate.cer');
+        $response = curl_exec($ch);
+
+        if (curl_errno($ch)) {
+            Log::info(curl_error($ch));
+        }
+
+        curl_close($ch);
+        return json_decode($response);
+    }
+
     public function confirmTransaction(Request $request)
     {
         $transaction = DB::table('mpesas') -> insert([
@@ -25,7 +45,79 @@ class TransactionController extends Controller
             'amount' => 5000, #floor(rand(100,1000)),
             'created_at'=> Carbon::now(),
             'updated_at' => Carbon::now()
-            ]);
-        echo $transaction . '\r\n'.'\n';
+        ]);
+        // echo $transaction . '\r\n'.'\n';
+    }
+
+    public function validateTransaction(Request $request)
+    {
+        Log::info($request -> all());
+        return response() -> json(json_encode([
+            'ResultCode' => 0,
+            'ResultDesc' => "Accepted"
+        ]));
+    }
+
+    public function registerUrl(Request $request)
+    {
+        $authorization = base64_encode(env('MPESA_CONSUMER_KEY') . ":" . env('MPESA_CONSUMER_SECRET'));
+        $authorization = $this -> getAccessToken() -> access_token;
+        Log::info($authorization);
+        $payload = [
+            'ShortCode' => '4115361',
+            'ResponseType' => 'Completed',
+            "ConfirmationURL" => "https://f63e-197-232-141-33.ngrok-free.app/api/mobile_money/confirmation",
+            "ValidationURL" => "https://f63e-197-232-141-33.ngrok-free.app/api/mobile_money/validation"
+        ];
+
+        $ch = curl_init('https://sandbox.safaricom.co.ke/mpesa/c2b/v2/registerurl');
+        curl_setopt($ch, CURLOPT_HTTPHEADER, [
+            'Authorization: Bearer ' . $authorization,
+            'Content-Type: application/json'
+        ]);
+        curl_setopt($ch, CURLOPT_POST, 1);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($payload));
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+
+        $response = curl_exec($ch);
+
+        Log::info($response);
+
+        if (curl_errno($ch)) {
+            Log::info(curl_error($ch));
+        }
+    }
+
+    public function simulate(Request $request)
+    {
+        $authorization = base64_encode(env('MPESA_CONSUMER_KEY') . ":" . env('MPESA_CONSUMER_SECRET'));
+        $authorization = $this -> getAccessToken() -> access_token;
+        $payload = [
+            'ShortCode' => '4115361',
+            'CommandID' => 'CustomerPayBillOnline',
+            'Amount' => 200,
+            'Msisdn' => '254708374149',
+            'BillRefNumber' => 'TestABC'
+        ];
+
+        $ch = curl_init('https://sandbox.safaricom.co.ke/mpesa/c2b/v1/simulate');
+
+        curl_setopt($ch, CURLOPT_HTTPHEADER, [
+            'Authorization: Bearer ' . $authorization,
+            'Content-Type: application/json'
+        ]);
+        curl_setopt($ch, CURLOPT_POST, 1);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($payload));
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+
+        $response = curl_exec($ch);
+
+        Log::info($response);
+
+        if (curl_errno($ch)) {
+            Log::info(curl_error($ch));
+        }
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,11 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
-Route::group(['prefix' => env('ROUTE_PREFIX'), 'middleware' => ['checkRequestIpMiddlware']], function(){
-    Route::get('/confirmation', [App\Http\Controllers\Api\TransactionController::class, 'confirmTransaction']) -> name('transaction.confirmation');
+// , 'middleware' => ['checkRequestIpMiddlware']
+Route::group(['prefix' => env('ROUTE_PREFIX')], function(){
+    Route::get('/credentials', [App\Http\Controllers\Api\TransactionController::class, 'getAccessToken']) -> name('transaction.access_token');
+    Route::get('/registerUrl', [App\Http\Controllers\Api\TransactionController::class, 'registerUrl']) -> name('transaction.register_url');
+    Route::post('/confirmation', [App\Http\Controllers\Api\TransactionController::class, 'confirmTransaction']) -> name('transaction.confirmation');
+    Route::post('/validation', [App\Http\Controllers\Api\TransactionController::class, 'validateTransaction']) -> name('transaction.validation');
+    Route::get('/simulate', [App\Http\Controllers\Api\TransactionController::class, 'simulate']) -> name('transaction.simulate');
 });
